b94a77a798f15ba6cfa2f136dd4cb6ce2471cdeb
anaso <ana.sofiia02@gmail.com>
2024-04-17 14:29:17 -0600

    Commit inicial.

---
+++ src/main/java/com/example/ucarecrud/UCareCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.ucarecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UCareCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UCareCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ucarecrud/UCareCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ucarecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UCareCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
