2c94909bd585640f945682f0ee460b31b5daf9d9
anaso <ana.sofiia02@gmail.com>
2024-04-18 12:17:10 -0600

    Modificación en clase "Actividad".

--- src/main/java/com/example/ucarecrud/modelo/Actividad.java
+++ src/main/java/com/example/ucarecrud/modelo/Actividad.java
@@ -1,10 +1,12 @@
 package com.example.ucarecrud.modelo;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 @Table(name = "actividades")
 public class Actividad {
     @Id
@@ -21,48 +23,8 @@ public class Actividad {
         this.fechaCreacion = LocalDateTime.now();
         this.completada = false;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public LocalDateTime getFechaCreacion() {
-        return fechaCreacion;
-    }
-
-    public void setFechaCreacion(LocalDateTime fechaCreacion) {
-        this.fechaCreacion = fechaCreacion;
-    }
-
-    public LocalDateTime getFechaLimite() {
-        return fechaLimite;
-    }
-
-    public void setFechaLimite(LocalDateTime fechaLimite) {
-        this.fechaLimite = fechaLimite;
-    }
-
-    public boolean isCompletada() {
-        return completada;
-    }
-
-    public void setCompletada(boolean completada) {
-        this.completada = completada;
-    }
 }
