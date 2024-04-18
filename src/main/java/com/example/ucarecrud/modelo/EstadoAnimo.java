27d8917b26cb1e73333f5e1de0714bb02ceb4500
cherry <cvmedina@uamv.edu.ni>
2024-04-17 22:48:03 -0600

    actualizacion

--- src/main/java/com/example/ucarecrud/modelo/EstadoAnimo.java
+++ src/main/java/com/example/ucarecrud/modelo/EstadoAnimo.java
@@ -4,15 +4,18 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
 
+import javax.validation.constraints.Size;
+
 @Entity
 @Data
 public class EstadoAnimo extends Ids {
     //enum dentro de estado de animo
-    public enum ESTADO {Feliz, Bien, Cansado, Estresado, Triste};
+    public enum ESTADO {Feliz, Bien, Cansado, Estresado, Triste}
 
     private ESTADO estado;
 
-    @Column(length = 240)
+    @Column(length = 140)
+    @Size(max = 140, message = "El comentario debe tener como máximo 140 caracteres")
     private String comentario;
 
 }
