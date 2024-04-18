13ce9055c99c12318f1617f70a69ee8833cb51c1
cherry <cvmedina@uamv.edu.ni>
2024-04-17 22:46:35 -0600

    actualizacion

--- src/main/java/com/example/ucarecrud/service/IServiceEstadoAnimo.java
+++ src/main/java/com/example/ucarecrud/service/IServiceEstadoAnimo.java
@@ -1,4 +1,15 @@
 package com.example.ucarecrud.service;
 
+import com.example.ucarecrud.modelo.EstadoAnimo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface IServiceEstadoAnimo {
+    public List<EstadoAnimo> getAll();
+
+    public void create(EstadoAnimo estadoAnimo);
+
+    public void delete(Integer id);
 }
