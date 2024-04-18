a804692786c57ab103c9fbd9bb3bd074f5d09777
cherry <cvmedina@uamv.edu.ni>
2024-04-17 22:49:22 -0600

    actualizacion de ids pero con integers

--- src/main/java/com/example/ucarecrud/modelo/Ids.java
+++ src/main/java/com/example/ucarecrud/modelo/Ids.java
@@ -1,24 +1,18 @@
 package com.example.ucarecrud.modelo;
-
-
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
+
 
 @MappedSuperclass
 @Data
 public class Ids {
+
     @Id
     // Anotación que especifica la generación automática del valor del identificador
-    @GeneratedValue(generator = "system-uuid")
-    // Configuración del generador de identificadores
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    // Configuración de la columna asociada al identificador en la base de datos
-    @Column(length = 32)
-    // Anotación que indica que este campo debe estar oculto en la interfaz de usuario
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //unique para que no se repita y nullable pa que no sea nulo
+    @Column(name = "id", nullable = false, unique = true)
+    private Integer id;
+
 
 }
